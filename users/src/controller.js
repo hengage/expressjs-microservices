61ccd413b8afb2d3fcb41e198e2f7e6623a2be2c
hengage <tunecarterkillz@gmail.com>
2023-01-16 15:47:20 +0100

    Feature: send messages queue on user registration

--- users/src/controller.js
+++ users/src/controller.js
@@ -1,13 +1,15 @@
 const User = require('./model')
+const { messageQueue } = require('./rabbitmq')
 
 const register = async (req, res) => {
     try {
         const newUser = new User(req.body)
-        console.log({newUser})
+        // console.log({newUser})
         await newUser.save()
         res.status(201).json({
              message: 'User successfully registered', newUser 
-            })
+        })
+        messageQueue('user.register', {newUser})
     } catch (error) {
         console.error({error})
         return res.status(500).json({message: error})
---
+++ users/src/rabbitmq.js
@@ -0,0 +1,32 @@
+const amqp = require('amqplib/callback_api')
+
+
+const messageQueue = (queuename, message) => {
+    amqp.connect('amqp://localhost:5672', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+
+        // Create a channel
+        connection.createChannel(async function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            // Declare a queue
+            channel.assertQueue(queuename, {
+                durable: false
+            });
+
+            await channel.sendToQueue(queuename, Buffer.from(JSON.stringify(message)));
+            console.log('Message sent:', message)
+            // setTimeout( () => {
+            //     connection.close();
+            //     process.exit(0)
+            // }, 500);
+        });
+    });
+}
+
+
+module.exports = { messageQueue }
